483f759085cc2567e25954b7787ee242d42247a4
Abbe <abbe.vass@gmail.com>
2024-09-16 09:51:44 +0200

    noice Abbe

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+        System.out.println("Hej från Abbe!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Vad heter du? ");
         String namn = scanner.nextLine();
