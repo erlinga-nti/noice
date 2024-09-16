137402943eea2154907d7c43be14c41e2e981268
erlinga-nti <erling.andersson@elev.ga.ntig.se>
2024-09-16 09:45:34 +0200

    noice.1

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Vad heter du? ");
+        String namn = scanner.nextLine();
+        System.out.println("Hej " + namn + "!");
+        scanner.close();
     }
 }
