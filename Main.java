e69cc458f37e229469055890267f78e7630e7e8e
erlinga-nti <erling.andersson@elev.ga.ntig.se>
2024-09-16 09:52:40 +0200

    corection

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hej från Abbe!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Vad heter du? ");
         String namn = scanner.nextLine();
         System.out.println("Hej " + namn + "!");
+        System.out.println("Hej från Abbe!");
         scanner.close();
     }
 }
